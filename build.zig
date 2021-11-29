ebe35bc9a9a2b50bff517149175382d24b742355
Gustavo Rehermann (Gustavo6046) <rehermann6046@gmail.com>
2021-11-29 17:13:52 -0300

    Separate library main and test

--- build.zig
+++ build.zig
@@ -1,19 +1,36 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+
+fn buildLibraryStep() *std.build.LibExeObjStep {
+
+}
+
+pub fn addToStep(b: *std.build.Builder, step: *std.build.LibExeObjStep) void {
+    step.linkLibrary(buildLibraryStep());
+}
+
+pub fn step(b: *std.build.Builder) *std.build.LibExeObjStep {
     // Standard release options allow the person running `zig build` to select
     // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+    const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
-    const lib = b.addStaticLibrary("zynets",
-        "src/main.zig"
-    );
-    lib.setBuildMode(mode);
-    lib.install();
+    const zynets = b.addStaticLibrary("zynets", "src/main.zig");
 
-    const main_tests = b.addTest("src/main.zig");
+    return zynets;
+}
+
+pub fn build(b: *Builder) void {
+    const mode = b.standardReleaseOptions();
+    const target = b.standardTargetOptions(.{});
+
+    const main_tests = b.addTest("src/test.zig");
     main_tests.setBuildMode(mode);
+    main_tests.setTarget(target);
+    link(b, main_tests, .{});
 
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&main_tests.step);
+
+    step(b);
 }
--- src/main.zig
+++ src/main.zig
@@ -4,7 +4,3 @@ const testing = std.testing;
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
-
-test "basic add functionality" {
-    try testing.expect(add(3, 7) == 10);
-}
